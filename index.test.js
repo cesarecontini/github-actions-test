dac2d94f06931bf8d4f64469f6e9350c8096a869
Cesare Giani Contini <cesarecontini@gmail.com>
2023-09-27 16:34:20 +0200

    Added app and tests

---
+++ index.test.js
@@ -0,0 +1,5 @@
+import utils from "./utils.js";
+
+test("Sums 2 numbers", () => {
+  expect(utils.sum(1, 2)).toBe(3);
+});
---
+++ utils.js
@@ -0,0 +1,5 @@
+const sum = (a, b) => a + b;
+
+const utils = { sum };
+
+export default utils;
